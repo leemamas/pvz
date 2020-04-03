293f5e6e34a65975c1346dda9b812d8126845639
leemamas <leemamas@admin.com>
2020-04-03 09:00:18 +0800

    创建豌豆对象

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import pygame, sys
+from plants.peashooter import Peashooter
 
 # 这初始化
+
 pygame.init()
 
 # 窗体大小
@@ -15,15 +17,25 @@ backgroup = pygame.image.load(bg_path).convert()
 
 # 主函数
 def main():
+    block = pygame.time.Clock()
+    peashooter = Peashooter()
+    index = 0
     while 1:
+
+        block.tick(30)
         # 绘制背景
         screen.blit(backgroup, (0, 0))
+        if index > 13:
+            index = 0
+        screen.blit(peashooter.images[index % 13], peashooter.rect)
 
         pygame.display.update()
 
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 sys.exit()
+
+        index += 1
 
 
 if __name__ == '__main__':
